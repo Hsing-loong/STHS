ccea678a61d074cb99456b751208139ca9f9390a
Hsing-loong <283028678@qq.com>
2018-12-04 21:52:45 +0800

    Update IGA.m

--- GA/IGA.m
+++ GA/IGA.m
@@ -11,6 +11,7 @@ D=length(xmin);%Number of dimensions
 pc=0.8;
 pm=0.05;
 Tc=0.8*itermax;
+repnum=0;
 
 
 x=x0;% individual initialization
@@ -18,6 +19,7 @@ x=x0;% individual initialization
 mergf=f;
 mergx=x;
 fgbest=zeros(itermax,size(f,2));
+
 for iter=1:itermax      
     epsilon = epsilonControl(f(:,1),iter,Tc);    
     gama=0.8*(xmax-xmin)-(iter-1)/(itermax-1)*0.5*(xmax-xmin);
@@ -26,6 +28,7 @@ for iter=1:itermax
     if repnum>popsize*0.01
         mutx=mutx+(rand(1,popsize)<pm).*(randi(2,D,popsize)-1).*trnd(1,D,popsize);
     end
+    mutx=(mutx>xmax).*xmax+(mutx<xmin).*xmax+(mutx<=xmax&mutx>=xmin).*mutx;
     [newf,newx]=Fitness(mutx);
     mergf=[f;newf];
     mergx=[x,newx];
