a2990dbc41f3fdd5c2c40b2baca8b10ecf61a674
Hsing-loong Lau <283028678@qq.com>
2018-12-05 03:19:46 +0800

    Update ObFunc_4Hydro1Thermal.m

--- 4hydro1thermal/ObFunc_4Hydro1Thermal.m
+++ 4hydro1thermal/ObFunc_4Hydro1Thermal.m
@@ -1,10 +1,14 @@
-function  [obvalue_viol,R,V,Q,Ph,SP,Ps]= ObFunc_4Hydro1Thermal(R)
+function  [obvalue_viol,V,R,Q,Ph,SP,Ps]= ObFunc_4Hydro1Thermal(V)
 global params;
 inflow=params.I;
 [T,Nh]=size(inflow);
-V=zeros(T+1,Nh);
-V(1,:)=params.Vini;
-V(end,:)=params.Vend;
+V0=zeros(T,Nh);
+V0(1,:)=params.Vini;
+V0(2:end,:)=V;
+V1=zeros(T,Nh);
+V1(1:end-1,:)=V;
+V1(end,:)=params.Vend;
+R=zeros(T,Nh);
 for i=1:Nh
     if i<3
         
@@ -16,47 +20,11 @@ for i=1:Nh
         inflow(:,i)=inflow(:,i)+...
             [zeros(params.Td(3),1);R(1:T-params.Td(3),3)];
     end
-    
-    array=1:T;
-    m=1;
-    while m<100
-        VT=params.Vini(i)+sum(inflow(:,i)-R(:,i));
-        deltaV=VT-params.Vend(i);
-        if abs(deltaV)<1e-5
-            break;
-        end
-        
-        k=length(array);
-%         k=randi(l);
-        index=array(randperm(k));
-        % Using the adjacent differences of sorted uniform RVs
-        if k==1
-            x=rand;
-        else
-            x = sort(rand(k-1,1));
-            x = vertcat(x(1,:),diff(x,1,1),1-x(end,:)); % the colums of x sum to 1
-        end
-        deltaR = deltaV*x; % shift-and-scale x to have column-sum of s
-        
-%         index=array;
-%         deltaR=deltaV/length(array);
-        if deltaV<0
-            R(index,i)=max(R(index,i)+deltaR,params.Qmin(index,i));
-            array=find(R(:,i)>params.Qmin(:,i));
-        else
-            R(index,i)=R(index,i)+deltaR;
-            array=1:T;
-        end
-        m=m+1;
-    end
+    R(:,i)=V0(:,i)+inflow(:,i)-V1(:,i);
 end
-for t=1:T
-    V(t+1,:)=V(t,:)+inflow(t,:)-R(t,:);
-end
-V=V(2:end,:);
 a=params.C2;
-b=params.C3.*V+params.C5;
-c=params.C1.*(V.^2)+params.C4.*V+params.C6;
+b=params.C3.*V1+params.C5;
+c=params.C1.*(V1.^2)+params.C4.*V1+params.C6;
 
 %note
 %1.Ph can not reach params.Phmax with any V1
@@ -66,16 +34,17 @@ discriminant=b.^2-4*a.*(c-params.Phmin);
 extrpoint=-0.5*b./a;
 Q1=0.5*(-b+sqrt(discriminant))./a;
 Q2=0.5*(-b-sqrt(discriminant))./a;
-Q=Q1.*(R<Q1)+R.*(R>=Q1&R<=Q2)+Q2.*(R>Q2);
-Q=params.Qmin.*(Q<params.Qmin)+Q.*(Q>=params.Qmin&Q<=params.Qmax)+params.Qmax.*(Q>params.Qmax);
-Q=Q.*(Q<extrpoint)+extrpoint.*(Q>=extrpoint);
+
+Qmin=max(Q1,params.Qmin);
+Qmax=min(min(Q2,params.Qmax),extrpoint);
+Q=R.*(R<=Qmax)+Qmax.*(R>Qmax);
 if params.PDZ
     logic=Q<params.Qpu&Q>params.Qpl;
     Q=Q.*(~logic)+params.Qpl.*(logic);
 end
 SP=R-Q;
-Ph=params.C1.*(V.^2)+params.C2.*(Q.^2)+params.C3.*(V.*Q)+...
-    params.C4.*V+params.C5.*Q+params.C6;
+Ph=params.C1.*(V1.^2)+params.C2.*(Q.^2)+params.C3.*(V1.*Q)+...
+    params.C4.*V1+params.C5.*Q+params.C6;
 Ps=params.load-sum(Ph,2);
 if params.VP
     cost=sum(params.a.*Ps.^2+params.b.*Ps+params.c+abs(params.e.*sin(params.f.*(params.Psmin-Ps))));
@@ -83,12 +52,14 @@ else
     cost=sum(params.a.*Ps.^2+params.b.*Ps+params.c);
 end
 
-VminViol=max(0,params.Vmin-V(1:end-1,:));
-VmaxViol=max(0,V(1:end-1,:)-params.Vmax);
-VendViol=max(0,abs(V(end,:)-params.Vend)-1e-5);
+VminViol=max(0,params.Vmin-V);
+VmaxViol=max(0,V-params.Vmax);
+% VendViol=max(0,abs(V(end,:)-params.Vend)-1e-5);
+RminViol=max(0,Qmin-R);
 PsminViol=max(0,params.Psmin-Ps);
 PsmaxViol=max(0,Ps-params.Psmax);
-viol=[VminViol(:);VmaxViol(:);VendViol(:);PsminViol(:);PsmaxViol(:)];
+
+viol=[VminViol(:);VmaxViol(:);RminViol(:);PsminViol(:);PsmaxViol(:)];
 viol=sum(viol);
 obvalue_viol=[viol,cost];
 end
