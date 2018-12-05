36ee8e5fbb8e047ace021b37882d1039c3b939ee
Hsing-loong <283028678@qq.com>
2018-12-05 17:36:03 +0800

    Update Initial.m

--- 4hydro1thermal/Initial.m
+++ 4hydro1thermal/Initial.m
@@ -15,20 +15,21 @@ for i=1:Nh
             [zeros(params.Td(3),1);R(1:T-params.Td(3),3)];
     end
     s=params.Vini(i)+sum(inflow(:,i))-params.Vend(i);
+        
     x = sort(rand(T-1,1));
     x = vertcat(x(1,:),diff(x,1,1),1-x(end,:)); % the colums of x sum to 1
     R(:,i) = params.Qmin(:,i)+(s-sum(params.Qmin(:,i)))*x; % shift-and-scale x to have column-sum of s
 end
-
+% x0=R(:);
 V=zeros(T+1,Nh);
 V(1,:)=params.Vini;
 V(end,:)=params.Vend;
 for t=1:T
     V(t+1,:)=V(t,:)+inflow(t,:)-R(t,:);
 end
-x0=V(2:end-1,:);
-x0(x0>params.Vmax)=params.Vmax(x0>params.Vmax);
-x0(x0<params.Vmin)=params.Vmin(x0<params.Vmin);
-x0=x0(:);
+V=V(2:end-1,:);
+logic=V>params.Vmax|V<params.Vmin;
+V=logic.*(params.Vmin+rand(T-1,Nh).*(params.Vmax-params.Vmin))+(~logic).*V;
+x0=V(:);
 end
 
