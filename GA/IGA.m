23e094effaeb5476f791ebb2fecb47c4d81168bb
Hsing-loong <283028678@qq.com>
2018-12-05 17:36:06 +0800

    Update IGA.m

--- GA/IGA.m
+++ GA/IGA.m
@@ -11,38 +11,66 @@ D=length(xmin);%Number of dimensions
 pc=0.8;
 pm=0.05;
 Tc=0.8*itermax;
+theta=0.9;
+cpmin=3;
+cpmax=10;
+Tlambda=0.95*Tc;
+elambda=1e-5;
 repnum=0;
 
 
 x=x0;% individual initialization
 [f,x]=Fitness(x);
-mergf=f;
-mergx=x;
-fgbest=zeros(itermax,size(f,2));
+viol=sortrows(f);
+epsilon0=viol(floor(theta*popsize),1);
+fgbest=zeros(itermax,2);
+for iter=1:itermax
 
-for iter=1:itermax      
-    epsilon = epsilonControl(f(:,1),iter,Tc);    
-    gama=0.8*(xmax-xmin)-(iter-1)/(itermax-1)*0.5*(xmax-xmin);
-    crox= GACrossover(f,x,mergf,mergx,pc,gama,epsilon);
-    mutx = GAMutation(crox,pm,xmin,xmax,iter,itermax);
-    if repnum>popsize*0.01
-        mutx=mutx+(rand(1,popsize)<pm).*(randi(2,D,popsize)-1).*trnd(1,D,popsize);
+
+
+    
+    viol=sortrows(f);
+    
+    lambda=1-sum(viol(:,1)>0)/popsize;
+    cp=cpmin+lambda*(cpmax-cpmin);
+    if iter>=Tc
+        epsilon=0;
+    else
+        epsilon=epsilon0*exp(-cp*(iter/Tc));
     end
-    mutx=(mutx>xmax).*xmax+(mutx<xmin).*xmax+(mutx<=xmax&mutx>=xmin).*mutx;
+    
+    [self,selx]= GASelection(f,x,epsilon);
+    
+    gama=0.8*(xmax-xmin)-(iter-1)/(itermax-1)*0.5*(xmax-xmin);  
+    crox= GACrossover(self,selx,f,x,pc,gama,epsilon);
+    
+    mutx = GAMutation(crox,pm,xmin,xmax,iter,itermax);
     [newf,newx]=Fitness(mutx);
     mergf=[f;newf];
     mergx=[x,newx];
+
+%     [unix,idx,~]=unique(mergx','rows','stable');
+%     unix=unix';
+%     unif=mergf(idx,:);
+    
     idx=BestRank(mergf,epsilon);
     fgbest(iter,:)=mergf(idx(1),:);
     xgbest=mergx(:,idx(1));
-    repnum=sum(ismember(mergx',xgbest','rows'));
     
-    [f,x]= GASelection(mergf,mergx, epsilon); 
-
-
+    renum=sum(ismember(mergx',xgbest','rows'));
+%     if renum>0
+%         rex=xgbest+(randi(2,D,popsize)-1).*trnd(1,D,popsize);
+%         [ref,rex]=Fitness(rex);
+%         mergx(:,end+1:end+renum)=rex;
+%         mergf(end+1:end+renum,:)=ref;
+%     end 
+    x=mergx(:,idx(1:popsize));
+    f=mergf(idx(1:popsize),:);
     
-        
-disp([num2str(iter),':',num2str(fgbest(iter,:))]);
+    
+    
+    
+    disp([num2str(iter),':',num2str([fgbest(iter,:),epsilon])]);
     
     
     
