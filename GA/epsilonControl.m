64792d4ee14efc5cbf5692926e77d5a7241cbffd
Hsing-loong <283028678@qq.com>
2018-12-05 17:36:00 +0800

    Update epsilonControl.m

--- GA/epsilonControl.m
+++ GA/epsilonControl.m
@@ -1,12 +1,12 @@
-function epsilon = epsilonControl(voil,iter,Tc)
+function epsilon = epsilonControl(epsilon0,iter,Tc)
 cpmin=3;
-cpmax=10;
-theta=0.9;
-popsize=length(voil);
-lambda=1-sum(voil>0)/popsize;
-cp=cpmin+lambda*(cpmax-cpmin);
-epsilon0=voil(floor(theta*popsize));
-if iter>Tc
+Tlambda=0.95*Tc;
+elambda=1e-5;
+cp=max(cpmin,(log(elambda)-log(epsilon0))/(1-Tlambda/Tc));
+if iter>Tlambda
+    cp=0.3*cp+0.7*cpmin;
+    epsilon=epsilon0*(1-iter/Tc)^cp;
+elseif iter>Tc
     epsilon=0;
 else
     epsilon=epsilon0*(1-iter/Tc)^cp;
