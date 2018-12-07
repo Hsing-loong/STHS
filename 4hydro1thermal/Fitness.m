2d92227d308d0845f798d0b74fc5d7eaae514f5f
Hsing-loong <283028678@qq.com>
2018-12-07 14:14:08 +0800

    Update Fitness.m

--- 4hydro1thermal/Fitness.m
+++ 4hydro1thermal/Fitness.m
@@ -1,9 +1,9 @@
-function [f,x] = Fitness(x)
-global params;
+function [f,x] = Fitness(x) 
+global funhandle;
 x=num2cell(x,1);
-V=cellfun(params.DecVarTrans1,x,'UniformOutput',false);
-[obvalue_viol,V,~,~,~,~,~]=cellfun(params.ObFunc,V,'UniformOutput',false);
-x=cellfun(params.DecVarTrans2,V,'UniformOutput',false);
+V=cellfun(funhandle.DecVarTrans1,x,'UniformOutput',false);
+[obvalue_viol,V,~,~,~,~,~]=cellfun(funhandle.ObFunc,V,'UniformOutput',false);
+x=cellfun(funhandle.DecVarTrans2,V,'UniformOutput',false);
 x=cell2mat(x);
 f=cell2mat(obvalue_viol');
 end
