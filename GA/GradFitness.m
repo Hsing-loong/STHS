92c88c104d550cbf22016fab2b3bc5fafac58d4d
Hsing-loong <283028678@qq.com>
2018-12-07 13:54:10 +0800

    Create GradFitness.m

---
+++ GA/GradFitness.m
@@ -0,0 +1,10 @@
+function [f,x] = GradFitness(x) 
+global funhandle;
+x=num2cell(x,1);
+V=cellfun(funhandle.DecVarTrans1,x,'UniformOutput',false);
+[obvalue_viol,~,~,~,~,~,~]=cellfun(funhandle.GradObFunc,V,'UniformOutput',false);
+x=cellfun(funhandle.DecVarTrans2,V,'UniformOutput',false);
+x=cell2mat(x);
+f=cell2mat(obvalue_viol');
+end
+
