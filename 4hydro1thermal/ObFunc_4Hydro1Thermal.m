110c75fba80570ce3ff372ff1915629528407300
Hsing-loong <283028678@qq.com>
2019-01-21 17:51:14 +0800

    Update ObFunc_4Hydro1Thermal.m

--- 4hydro1thermal/ObFunc_4Hydro1Thermal.m
+++ 4hydro1thermal/ObFunc_4Hydro1Thermal.m
@@ -8,7 +8,7 @@ V0(1,:)=params.Vini;
 
 V1=zeros(T,Nh);
 V1(end,:)=params.Vend;
-R=Q;
+R=params.Qmin.*(Q<=params.Qmin)+Q.*(Q>params.Qmin);
 for i=1:Nh
     if i<3
         
@@ -20,47 +20,51 @@ for i=1:Nh
         inflow(:,i)=inflow(:,i)+...
             [zeros(params.Td(3),1);R(1:T-params.Td(3),3)];
     end
-%     m=1;
-%     t=randperm(T);
-%     while m<T+1
-%         VT=params.Vini(i)+sum(inflow(:,i)-R(:,i));
-%         deltaV=VT-params.Vend(i);
-%         if deltaV<-eps
-%             R(t(m),i)=max(R(t(m),i)+deltaV,params.Qmin(t(m),i));
-%         elseif deltaV>eps            
-%             R(t(m),i)=R(t(m),i)+deltaV;    
-%         else
-%             break;
-%         end
-%         m=m+1;
-%     end
-%     for t=1:T-1
-%         V1(t,i)=V0(t,i)+inflow(t,i)-R(t,i);
-%         V0(t+1,i)=V1(t,i);
-%     end  
-    while 1
-        R(R(:,i)<params.Qmin(:,i),i)=params.Qmin(R(:,i)<params.Qmin(:,i),i);
+    m=1;
+    t=randperm(T);
+    while m<T+1
         VT=params.Vini(i)+sum(inflow(:,i)-R(:,i));
         deltaV=VT-params.Vend(i);
         if deltaV<-eps
-            num=sum(R(:,i)>params.Qmin(:,i));
-            x = sort(rand(num-1,1));
-            x = vertcat(x(1),diff(x,1),1-x(end));
-            x = deltaV*x;
-            R(R(:,i)>params.Qmin(:,i),i)=R(R(:,i)>params.Qmin(:,i),i)+x;
-        elseif deltaV>eps
-            x = sort(rand(T-1,1));
-            x = vertcat(x(1),diff(x,1),1-x(end));
-            x = deltaV*x;
-            R(:,i)=R(:,i)+x;    
+            R(t(m),i)=max(R(t(m),i)+deltaV,params.Qmin(t(m),i));
+        elseif deltaV>eps            
+            R(t(m),i)=R(t(m),i)+deltaV;    
         else
             break;
         end
+        m=m+1;
     end
     for t=1:T-1
         V1(t,i)=V0(t,i)+inflow(t,i)-R(t,i);
         V0(t+1,i)=V1(t,i);
-    end
+    end  
+%     while 1
+%         VT=params.Vini(i)+sum(inflow(:,i)-R(:,i));
+%         deltaV=VT-params.Vend(i);
+%         if deltaV<-eps(params.Vend(i))
+%             idx=find(R(:,i)>params.Qmin(:,i));
+%             num=length(idx);
+%             if num>0
+%                 x = [0;sort(rand(num-1,1));1];
+%                 x = diff(x);
+%                 x = deltaV*x;
+%                 R(idx,i)=max(R(idx,i)+x,params.Qmin(idx,i));
+%             else
+%                 break;
+%             end
+%         elseif deltaV>eps(params.Vend(i))
+%             x = [0;sort(rand(T-1,1));1];
+%             x = diff(x);
+%             x = deltaV*x;
+%             R(:,i)=R(:,i)+x;
+%         else
+%             break;
+%         end
+%     end
+%     for t=1:T-1
+%         V1(t,i)=V0(t,i)+inflow(t,i)-R(t,i);
+%         V0(t+1,i)=V1(t,i);
+%     end
 
 end
 
@@ -81,7 +85,7 @@ Q2=0.5*(-b-sqrt(discriminant))./a;
 %
 Qmin=max(Q1,params.Qmin);
 Qmax=min(min(Q2,params.Qmax),extrpoint);
-Q=params.Qmin.*(R<Qmin)+R.*(R>=Qmin&R<=Qmax)+Qmax.*(R>Qmax);
+Q=Qmin.*(R<Qmin)+R.*(R>=Qmin&R<=Qmax)+Qmax.*(R>Qmax);
 % Q=R.*(R<=Qmax)+Qmax.*(R>Qmax);
 if params.PDZ
     logic=Q<params.Qpu&Q>params.Qpl;
@@ -97,17 +101,17 @@ else
     cost=sum(params.a.*Ps.^2+params.b.*Ps+params.c);
 end
 
-VminViol=max(0,params.Vmin-V1(1:end-1,:)-1e-5);
-VmaxViol=max(0,V1(1:end-1,:)-params.Vmax-1e-5);
-VendViol=max(0,abs(V1(end,:)-params.Vend)-1e-5);
-RminViol=max(0,params.Qmin-R-1e-5);
-% SPViol=max(0,0-SP);
-PhminViol=max(0,params.Phmin-Ph-1e-5);
-PhmaxViol=max(0,Ph-params.Phmax-1e-5);
-PsminViol=max(0,params.Psmin-Ps-1e-5);
-PsmaxViol=max(0,Ps-params.Psmax-1e-5);
+VminViol=max(0,params.Vmin-V1(1:end-1,:));
+VmaxViol=max(0,V1(1:end-1,:)-params.Vmax);
+VendViol=max(0,abs(V1(end,:)-params.Vend));
+% RminViol=max(0,params.Qmin-R-1e-5);
+SPViol=max(0,0-SP);
+PhminViol=max(0,params.Phmin-Ph);
+PhmaxViol=max(0,Ph-params.Phmax);
+PsminViol=max(0,params.Psmin-Ps);
+PsmaxViol=max(0,Ps-params.Psmax);
 
-viol=[VminViol(:);VmaxViol(:);VendViol(:);RminViol(:);PhminViol(:);PhmaxViol(:);PsminViol(:);PsmaxViol(:)];
+viol=[VminViol(:);VmaxViol(:);VendViol(:);SPViol(:);PhminViol(:);PhmaxViol(:);PsminViol(:);PsmaxViol(:)];
 viol=sum(viol);
 obvalue_viol=[viol,cost];
 end
